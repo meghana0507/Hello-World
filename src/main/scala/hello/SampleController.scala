5c2c458a831efd9074d48814fa8177a11d435a35
Meghana Kothaluri <kothaluri07@gmail.com>
2015-02-11 17:56:21 -0800

    SampleController.scala changed

--- src/main/scala/hello/SampleController.scala
+++ src/main/scala/hello/SampleController.scala
@@ -6,7 +6,7 @@ import org.springframework.boot;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.ResponseBody; 
 import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestMethod;
  
 @Controller
 @Configuration
